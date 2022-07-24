b924c738b75cdbece0d5ca59e87ab74345c92268
Carlos <carlos.barahona.t@usach.cl>
2022-07-24 01:34:49 -0400

    Configurated PM2 ecosystem file and updated backend submodules

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,38 @@
 module.exports = {
-  apps : [{
-    name   : "Neurone Forms",
-    script : "cd ./backend/neurone-forms/ && npm install && tsc && node dist/app.js"
+  apps : [
+  {
+    name   : "Neurone Auth",
+    script : "cd backend/neurone-auth/ && npm install && tsc && node dist/app.js",
+    env    : {
+      "PORT": "3005",
+      "SECRET_KEY": "placeholder",
+      "SALT_ROUNDS": "10",
+      "DB": "mongodb://127.0.0.1:27017/neurone"
+    }
+  },
+  {
+    name   : "Neurone Profile",
+    script : "cd ./backend/neurone-profile/ && npm install && tsc && node dist/app.js",
+    env    : {
+      "PORT": "3002",
+      "NEURONE_AUTH_PORT": "3005",
+      "DB": "mongodb://127.0.0.1:27017/neurone"
+    }
   },
   {
     name   : "Neurone Search",
-    script : "cd backend/neurone-search/ && npm install && tsc && node dist/app.js"
-  }]
+    script : "cd backend/neurone-search/ && npm install && tsc && node dist/app.js",
+    env    : {
+      "PORT":"3001",
+      "DB": "mongodb://127.0.0.1:27017/neurone",
+      "NEURONE_ASSET_PATH": "./assets",
+      "NEURONE_IFUCO_SORT_DISABLE": "true",
+      "NEURONE_SEARCH_ENGINE": "solr",
+
+      "NEURONE_SOLR_HOST": "localhost",
+      "NEURONE_SOLR_PORT": "8983",
+      "NEURONE_SOLR_CORE": "neurone"
+    }
+  }
+  ]
 }
